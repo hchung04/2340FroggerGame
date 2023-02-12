561a1344b129a5daba447100cb7cb1023c6b7d60
mhou36 <mhou36@gatech.edu>
2023-02-11 23:06:08 -0500

    Allowed user to continue to game screen after valid name input

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,19 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button switchToConfigurationActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchToConfigurationActivity = (Button) findViewById(R.id.startButton);
+        switchToConfigurationActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                switchToConfigurationActivity();
+            }
+        });
     }
 
-    public void changeToConfigurationScreen(View view) {
-        setContentView(R.layout.activity_configuration_screen);
+    private void switchToConfigurationActivity() {
+        Intent switchActivityIntent = new Intent(this, ConfigurationActivity.class);
+        startActivity(switchActivityIntent);
     }
 }
