4cc761b2f65e450f0be0406b69ac8ea7cabea678
eshen38 <eshen38@gatech.edu>
2023-02-13 10:10:00 -0500

    Added player selection on configuration screen

--- app/src/main/java/com/example/myapplication/ConfigurationActivity.java
+++ app/src/main/java/com/example/myapplication/ConfigurationActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -18,11 +22,69 @@ public class ConfigurationActivity extends AppCompatActivity {
     private EditText nameInput;
     private Button submit;
     private String gameDifficulty;
+    private Drawable chosenSprite;
+    private LinearLayout currentSpriteBg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        ImageView sprite1 = (ImageView) findViewById(R.id.sprite1);
+        LinearLayout sprite1Bg = (LinearLayout) findViewById(R.id.sprite1Bg);
+        currentSpriteBg = sprite1Bg;
+        sprite1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                resetSpriteChoices();
+                sprite1Bg.setBackgroundColor(Color.parseColor("#C7B8FF"));
+                chosenSprite = sprite1.getDrawable();
+                currentSpriteBg = sprite1Bg;
+            }
+        });
+
+        ImageView sprite2 = (ImageView) findViewById(R.id.sprite2);
+        LinearLayout sprite2Bg = (LinearLayout) findViewById(R.id.sprite2Bg);
+        sprite2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                resetSpriteChoices();
+                sprite2Bg.setBackgroundColor(Color.parseColor("#C7B8FF"));
+                chosenSprite = sprite2.getDrawable();
+                currentSpriteBg = sprite2Bg;
+            }
+        });
+
+        ImageView sprite3 = (ImageView) findViewById(R.id.sprite3);
+        LinearLayout sprite3Bg = (LinearLayout) findViewById(R.id.sprite3Bg);
+        sprite3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                resetSpriteChoices();
+                sprite3Bg.setBackgroundColor(Color.parseColor("#C7B8FF"));
+                chosenSprite = sprite3.getDrawable();
+                currentSpriteBg = sprite3Bg;
+            }
+        });
+
+        ImageView sprite4 = (ImageView) findViewById(R.id.sprite4);
+        LinearLayout sprite4Bg = (LinearLayout) findViewById(R.id.sprite4Bg);
+        sprite4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                resetSpriteChoices();
+                sprite4Bg.setBackgroundColor(Color.parseColor("#C7B8FF"));
+                chosenSprite = sprite4.getDrawable();
+                currentSpriteBg = sprite4Bg;
+            }
+        });
+
+        ImageView sprite5 = (ImageView) findViewById(R.id.sprite5);
+        LinearLayout sprite5Bg = (LinearLayout) findViewById(R.id.sprite5Bg);
+        sprite5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                resetSpriteChoices();
+                sprite5Bg.setBackgroundColor(Color.parseColor("#C7B8FF"));
+                chosenSprite = sprite5.getDrawable();
+                currentSpriteBg = sprite5Bg;
+            }
+        });
 
         submit = (Button) findViewById(R.id.submitButton);
         submit.setOnClickListener(new View.OnClickListener() {
@@ -36,23 +98,22 @@ public class ConfigurationActivity extends AppCompatActivity {
                             ((RadioButton) findViewById(rg.getCheckedRadioButtonId()))
                                     .getText().toString();
                 }
-                if (isNameValid(name) && gameDifficulty != null) {
-                    switchToGameActivity(name, gameDifficulty);
-                } else if (!isNameValid(name)) {
+
+                if (checkNameValidity(name) && gameDifficulty != null && chosenSprite != null) {
+                    switchToGameActivity();
+                } else if (!checkNameValidity(name)) {
                     showInvalidNamePopup();
                 } else if (gameDifficulty == null) {
                     showInvalidDifficulty();
+                } else if (chosenSprite == null) {
+                    showInvalidSprite();
                 }
-
-
-
-
             }
         });
 
     }
 
-    private boolean isNameValid(String name) {
+    private boolean checkNameValidity(String name) {
         return !(name.trim().equals(""));
     }
     private void showInvalidNamePopup() {
@@ -73,11 +134,21 @@ public class ConfigurationActivity extends AppCompatActivity {
         toast.show();
     }
 
-    //Sends name here to GameActivity
-    private void switchToGameActivity(String name, String gameDifficulty) {
+    private void resetSpriteChoices() {
+        currentSpriteBg.setBackgroundColor(Color.parseColor("#FFFFFF"));
+    }
+
+    private void showInvalidSprite() {
+        Context context = getApplicationContext();
+        CharSequence text = "Don't forget to choose your player!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    private void switchToGameActivity() {
         Intent switchActivityIntent = new Intent(this, GameActivity.class);
-        switchActivityIntent.putExtra("name_key", name);
-        switchActivityIntent.putExtra("level_key", this.gameDifficulty);
         startActivity(switchActivityIntent);
     }
 }
