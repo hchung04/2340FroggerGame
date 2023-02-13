62a4fd73ab307818de5290f42e642e27c75b0f1e
Shirley <92553759+shirleylin383@users.noreply.github.com>
2023-02-12 22:42:17 -0500

    added in points and lives for display

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -2,13 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
+    TextView pointsCounter;
+    TextView livesCounter;
+
+    int points = 0;
+    int livesRemaining = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        pointsCounter = (TextView) findViewById(R.id.pointCounter);
+        livesCounter = (TextView) findViewById(R.id.livesCounter);
+
+        pointsCounter.setText("POINTS:" + points);
+        livesCounter.setText("LIVES: " + livesRemaining);
+
+
+
+
     }
 }
