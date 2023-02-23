82b8205354dfbcd3915b05fb78f362017b64f34c
mhou36 <mhou36@gatech.edu>
2023-02-23 02:12:09 -0500

    Updated grid to be 11 x 8 using LinearLayout instead, with a player's ability to jump to the next row above

---
+++ app/src/main/java/com/example/myapplication/Grass.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import android.widget.ImageView;
+
+public class Grass {
+
+    ImageView view;
+
+    public Grass(ImageView view) {
+        this.view = view;
+    }
+}
