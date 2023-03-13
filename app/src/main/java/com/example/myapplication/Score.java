6915ae04ccdffd86bdcd4341461401ab064910ce
mhou36 <mhou36@gatech.edu>
2023-03-13 06:54:37 -0400

    Updated checkstyle errors

--- app/src/main/java/com/example/myapplication/Score.java
+++ app/src/main/java/com/example/myapplication/Score.java
@@ -4,9 +4,9 @@ public class Score {
     private float maxDistance;
     private int score;
 
-    private final int TILE = 137;
+    //private final int TILE = 137;
 
-    public Score(){
+    public Score() {
         maxDistance = 0; //fill w/ starting y coord
         score = 0;
     }
@@ -14,12 +14,12 @@ public class Score {
     public int updateScore(float yCoord) {
         if (yCoord < maxDistance) {
             maxDistance = yCoord;
-            int row = -1*(int)(yCoord/137);
-            if (row == 1){ //modify these row values depending on where cars go
+            int row = -1 * (int) (yCoord / 137);
+            if (row == 1) { //modify these row values depending on where cars go
                 score += 2;
-            } else if (row == 2){
+            } else if (row == 2) {
                 score += 3;
-            } else if (row == 3){
+            } else if (row == 3) {
                 score += 4;
             } else {
                 score++;
@@ -28,7 +28,7 @@ public class Score {
         return score;
     }
 
-    public int getScore(){
+    public int getScore() {
         return score;
     }
 }
