a579bc9033fb83b690fa4481c912203abacaf2bf
hchung313 <hchung313@gatech.edu>
2023-04-16 23:47:40 -0400

    Got rid of middle grass tiles.

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -90,11 +90,7 @@ public class GameActivity extends AppCompatActivity {
                 if (rowNum == 6 || rowNum == 5 || rowNum == 4) {
                     tileType = R.drawable.road_tile;
                 } else if (rowNum % 2 == 0 || rowNum == 3) {
-                    if (colNum == 3) {
-                        tileType = R.drawable.grass__0;
-                    } else {
                         tileType = R.drawable.water;
-                    }
                 } else {
                     tileType = R.drawable.grass__0;
                 }
