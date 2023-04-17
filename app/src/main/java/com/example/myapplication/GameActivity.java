5cee3ef5a5f3cf56dff3909bd58014b221fdff58
hchung313 <hchung313@gatech.edu>
2023-04-16 23:29:23 -0400

    Fixed dying when on log but touches water

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -203,7 +203,7 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void updateStats(Sprite sprite, Score score) {
-        if (sprite.checkWater()) {
+        if (sprite.checkWater() && !(collided && (collidedObject instanceof Log))) {
             if (sprite.getLivesRemaining() > 1) {
                 pointsCounter.setText("Points: " + score.subtractScore());
                 sprite.resetToStart();
